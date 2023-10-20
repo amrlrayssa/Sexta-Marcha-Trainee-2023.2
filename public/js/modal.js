6b9912100d721609f4ed7e2f5decc76d9cbc383d
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-10-20 20:06:53 -0300

    add css para os froms posts

---
+++ public/js/modal.js
@@ -0,0 +1,17 @@
+// var Cadastro = document.getElementById("modal");
+// var btn = document.getElementById("open-modal");
+// var form = document.getElementsByClassName("close")[0];
+
+// btn.onclick = function (){
+//     Cadastro.style.display = "block";
+// }
+
+// form.onclick = function () {
+//     Cadastro.style.display = "none";
+// }
+
+// window.onclick = function () {
+//     if( event.target == Cadastro){
+//         Cadastro.style.display = "none";
+//     }
+// }
