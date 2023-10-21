ca83e12afeb38382e63fe70d4e1f3fec40483b7c
Joao Victor Dias <terci_300@outlook.com>
2023-10-21 00:16:22 -0300

    sem erro na abertura

--- public/js/modal.js
+++ public/js/modal.js
@@ -17,24 +17,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
-
-
-
-/*
-var Cadastro = document.getElementById("modal");
-var btn = document.getElementById("create");
-var form = document.querySelector("close");
-
-btn.onclick = function () {
-    Cadastro.style.display = "block";
-}
-
-form.onclick = function () {
-    Cadastro.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target == Cadastro) {
-        Cadastro.style.display = "none";
-    }
-}*/
