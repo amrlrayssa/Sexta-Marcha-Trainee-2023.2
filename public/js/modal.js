636554fd17dd034b23d18c4d5809b3a6fd323c8b
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2023-10-21 17:48:13 -0300

    Adiciona modal de edição de post

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,40 +1,47 @@
     
+    var conteudo = document.getElementById("conteiner");
     var cadastro = document.getElementById("modal");
     var btn = document.getElementById("openCreatPost");
+    var btnT = document.getElementById("alsocreat");
     var btnM = document.getElementById("openCreatPostM");
     var form = document.getElementsByClassName("close")[0];
-
-    btn.onclick = function () {
-        cadastro.style.display = "block";
-    }
-
-    btnM.onclick = function () {
-        cadastro.style.display = "block";
-    }
-
-    form.onclick = function () {
-        cadastro.style.display = "none";
-    }
-
-    window.onclick = function () {
-        if (event.target == cadastro) {
-            cadastro.style.display = "none";
-        }
-    }
-
     var edit = document.getElementById("modal_edicao");
     var btnedit = document.getElementById("openEditPost");
     var btneditM = document.getElementById("openEditPostM");
     var formedit = document.getElementsByClassName("closeEdit")[0];
 
+
+    btn.onclick = function () {
+        cadastro.style.display = "block";
+        conteudo.style.filter = "brightness(30%)";
+    }
+
+    btnM.onclick = function () {
+        cadastro.style.display = "block";
+        conteudo.style.filter = "brightness(30%)";
+    }
+
+    form.onclick = function () {
+        cadastro.style.display = "none";
+        conteudo.style.filter = "brightness(100%)";
+    }
+
+
     btnedit.onclick = function () {
         edit.style.display = "block";
+        conteudo.style.filter = "brightness(30%)";
     }
 
     btneditM.onclick = function () {
-        cadastro.style.display = "block";
+        edit.style.display = "block";
+        conteudo.style.filter = "brightness(30%)";
+
     }
 
     formedit.onclick = function () {
         edit.style.display = "none";
+        conteudo.style.filter = "brightness(100%)";
     }
+
+   
+    
