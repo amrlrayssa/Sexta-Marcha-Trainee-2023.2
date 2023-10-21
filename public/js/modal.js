04a3fec6238b56b448807cf90ea35ddd92938d18
Joao Victor Dias <terci_300@outlook.com>
2023-10-21 00:01:15 -0300

    teste1 modal create_post

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,17 +1,40 @@
-// var Cadastro = document.getElementById("modal");
-// var btn = document.getElementById("open-modal");
-// var form = document.getElementsByClassName("close")[0];
+document.addEventListener("DOMContentLoaded", function() {
+    var Cadastro = document.getElementById("modal");
+    var btn = document.getElementById("create");
+    var form = document.getElementsByClassName("close")[0];
 
-// btn.onclick = function (){
-//     Cadastro.style.display = "block";
-// }
+    btn.onclick = function () {
+        Cadastro.style.display = "block";
+    }
 
-// form.onclick = function () {
-//     Cadastro.style.display = "none";
-// }
+    form.onclick = function () {
+        Cadastro.style.display = "none";
+    }
 
-// window.onclick = function () {
-//     if( event.target == Cadastro){
-//         Cadastro.style.display = "none";
-//     }
-// }
+    window.onclick = function (event) {
+        if (event.target == Cadastro) {
+            Cadastro.style.display = "none";
+        }
+    }
+});
+
+
+
+/*
+var Cadastro = document.getElementById("modal");
+var btn = document.getElementById("create");
+var form = document.querySelector("close");
+
+btn.onclick = function () {
+    Cadastro.style.display = "block";
+}
+
+form.onclick = function () {
+    Cadastro.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == Cadastro) {
+        Cadastro.style.display = "none";
+    }
+}*/
