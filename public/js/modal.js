76711a91046c1039a661348deb6fac9838bb4c59
Breno Furtado Rosado <breno.rosado@estudante.ufjf.br>
2023-10-22 13:35:59 -0300

    Adiciona Modal de excluir posts

--- public/js/modal.js
+++ public/js/modal.js
@@ -9,6 +9,10 @@
     var btnedit = document.getElementById("openEditPost");
     var btneditM = document.getElementById("openEditPostM");
     var formedit = document.getElementsByClassName("closeEdit")[0];
+    var btnrmv = document.getElementById("openRmvPost");
+    var btnrmvM = document.getElementById("openRmvPostM");
+    var rmv = document.getElementById("modal_rmv");
+    var formrmv = document.getElementsByClassName ("closermv")[0];
 
 
     btn.onclick = function () {
@@ -23,9 +27,9 @@
 
     form.onclick = function () {
         cadastro.style.display = "none";
+        edit.style.display = "none";
         conteudo.style.filter = "brightness(100%)";
     }
-
 
     btnedit.onclick = function () {
         edit.style.display = "block";
@@ -40,8 +44,36 @@
 
     formedit.onclick = function () {
         edit.style.display = "none";
+        cadastro.style.display = "none";
         conteudo.style.filter = "brightness(100%)";
     }
 
-   
+  
+    btnrmv.onclick = function () {
+        rmv.style.display = "block";
+        conteudo.style.filter = "brightness(30%)";
+    }
+
+    btnrmvM.onclick = function () {
+        rmv.style.display = "block";
+        conteudo.style.filter = "brightness(30%)";
+    }
+  
+    formrmv.onclick = function () {
+        rmv.style.display = "none";
+        edit.style.display = "none";
+        cadastro.style.display = "none";
+        conteudo.style.filter = "brightness(100%)";
+    }
+  
+  
+    window.onclick = function () {
+        if (event.target == conteudo && event.target != btn && event.target != btnT && event.target != btnedit && event.target != btneditM) {
+        if (event.target != btnrmv && event.target != btnrmv) {
+            cadastro.style.display = "none";
+            edit.style.display = "none";
+            rmv.style.display = "none";
+            conteudo.style.filter = "brightness(100%)";
+        }}
+   }
     
