759f390e431e729a3d3bda5d8fe24bad20f107c0
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-11-11 17:28:14 -0300

    ignore esse commit

--- public/js/modais.js
+++ public/js/modais.js
@@ -1,63 +1,55 @@
+// modais.js
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Selecionando elementos do DOM
-    var modalCriar = document.getElementById("modal");
-    var modalEdicao = document.getElementById("modal_edicao");
-    var modalExcluir = document.getElementById("modal_rmv");
-    var page = document.getElementById("page");
 
-    // Função para abrir o modal
-    function abrirModal(modal) {
-        modal.style.display = "block";
-        page.style.filter = "brightness(50%)";
-    }
+    // Seleciona os botões "Visualizar" e "Editar" e adiciona ouvintes de eventos
+    const viewButtons = document.querySelectorAll('.view-btn');
+    const editButtons = document.querySelectorAll('.btn-outline-info');
 
-    // Botões para abrir os modais
-    var criarUsuarioBtn = document.querySelector(".btn-outline-warning");
-    var criarPostBtn = document.querySelector(".button2");
-    var editarBtns = document.querySelectorAll("#edit, #edit-icon");
-    var excluirBtns = document.querySelectorAll(".btn-outline-danger");
-
-    criarUsuarioBtn.onclick = function () {
-        abrirModal(modalCriar);
-    };
-
-    criarPostBtn.onclick = function () {
-        abrirModal(modalCriar);
-    };
-
-    editarBtns.forEach(function (editarBtn) {
-        editarBtn.onclick = function () {
-            abrirModal(modalEdicao);
-        };
+    // Adiciona um ouvinte de evento para cada botão "Visualizar"
+    viewButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            // Obtém o valor do atributo data-target para identificar qual modal abrir
+            const targetModalId = button.getAttribute('data-target');
+            const targetModal = document.getElementById(targetModalId);
+            
+            // Exibe o modal de visualização
+            targetModal.style.display = 'block';
+        });
     });
 
-    excluirBtns.forEach(function (excluirBtn) {
-        excluirBtn.onclick = function () {
-            abrirModal(modalExcluir);
-        };
+    // Adiciona um ouvinte de evento para cada botão "Editar"
+    editButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            // Obtém o modal de edição
+            const editModal = document.getElementById('modal_edicao');
+            
+            // Exibe o modal de edição
+            editModal.style.display = 'block';
+        });
     });
 
-    // Função para fechar os modais
-    function fecharModais() {
-        modalCriar.style.display = "none";
-        modalEdicao.style.display = "none";
-        modalExcluir.style.display = "none";
-        page.style.filter = "brightness(100%)";
-    }
+    // Seleciona os botões "Cancelar" e adiciona ouvintes de eventos para fechar os modais
+    const cancelButtons = document.querySelectorAll('.close, .closeEdit, .Cancelar, .closermv');
 
-    // Botões para fechar modais
-    var closeBtns = document.querySelectorAll(".close, .closeEdit, .closermv");
-
-    closeBtns.forEach(function (closeBtn) {
-        closeBtn.onclick = fecharModais;
+    // Adiciona um ouvinte de evento para cada botão "Cancelar"
+    cancelButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            // Obtém o modal associado ao botão "Cancelar"
+            const modalToClose = button.closest('.modal');
+            
+            // Oculta o modal
+            modalToClose.style.display = 'none';
+        });
     });
 
-    // Evento para fechar os modais quando clicar fora deles
-    window.onclick = function (event) {
-        if (event.target === modalCriar || event.target === modalEdicao || event.target === modalExcluir) {
-            fecharModais();
-        }
-    };
+    // Adiciona um ouvinte de evento ao botão "Excluir" para realizar a ação de exclusão
+    const btnExcluir = document.getElementById('excluir');
+    btnExcluir.addEventListener('click', function () {
+        // Adicione aqui a lógica para excluir o post (por exemplo, enviar uma solicitação ao servidor)
+        // Após a exclusão, você pode ocultar o modal e realizar outras ações necessárias
+        const modalRmv = document.getElementById('modal_rmv');
+        modalRmv.style.display = 'none';
+    });
 
-    // Adicione outras funcionalidades/modificações conforme necessário
 });
