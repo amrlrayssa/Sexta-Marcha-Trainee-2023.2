e982d56f58b13cfe27e6dba9c0f3c2979f20cfac
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-11-30 23:14:39 -0300

    Revert "Merge branch 'main' into crud_login"
    This reverts commit 2d49214e5d119c7a11feb0a0651099973bc61a46, reversing
    changes made to 5d5ebe5b0f2b49098ae3062c436e6ac181341c98.

--- app/routes.php
+++ app/routes.php
@@ -6,4 +6,22 @@ use App\Core\Router;
 
     $router->get('', 'ExampleController@index');
 
+<<<<<<< HEAD
+=======
+
+    $router->get('admin', 'AdminController@view');
+
+    $router->post('admin/lista_usuarios/create', 'AdminController@create');
+    $router->post('admin/lista_usuarios/delete', 'AdminController@delete');
+    $router->post('admin/lista_usuarios/update', 'AdminController@edit');
+
+    $router->get('login', 'LoginController@view');
+    $router->post('logon', 'LoginController@login');
+    $router->get('logout', 'LoginController@logout');
+
+    $router -> get('admin_dashboard', 'dashboardController@view');
+
+
+
+>>>>>>> parent of 2d49214 (Merge branch 'main' into crud_login)
 ?>
--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -28,4 +28,77 @@ class QueryBuilder
             die($e->getMessage());
         }
     }
+<<<<<<< HEAD
+=======
+
+    public function insert($table,  $parameters){
+        $sql = sprintf(
+        'INSERT INFO %s (%s) value (%s)', 
+        $table, implode(', ', array_keys($parameters)),
+         ':' . implode(', :', array_keys($parameters))
+        );
+
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($parameters);
+
+
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function delete($table, $id){
+        $sql = sprintf(
+            'DELETE FROM  %s WHERE %s value', 
+            $table, 
+            'id = id'
+            );
+            try {
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute(compact('id'));
+    
+    
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        
+    }
+
+    public function edit($table, $id, $parameters){
+        $sql = sprintf(  
+            'UPDATE %s SET  WHERE %s',
+            $table,
+            implode(', ', array_map(function ($parameters){
+               
+              return "{$parameters} = :{$parameters}";
+            }, array_keys($parameters))),
+            'id = :id'
+        );
+        $parameters['$id'] = $id;
+        try {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute($parameters);
+
+
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    
+    }
+
+        function login($table, $email, $senha)
+        {
+            $sql = sprintf('SELECT * FROM %s WHERE email = :email', $table);
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->execute();
+            $user = $stmt->fetch();
+            if ($user && is_array($user) && $senha == $user['senha']) { 
+                return true;
+            } else {
+                return false;
+            }
+        }
+>>>>>>> parent of 2d49214 (Merge branch 'main' into crud_login)
 }
