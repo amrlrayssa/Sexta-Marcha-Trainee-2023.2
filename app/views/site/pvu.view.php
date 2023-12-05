bf2659554d5689999235457055a920852e3b3463
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-05 19:49:33 -0300

    add pvu

--- app/views/site/pvu.view.php
+++ app/views/site/pvu.view.php
@@ -1,4 +1,7 @@
 
+<?php  
+    
+ ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,16 +18,20 @@
 <body>
 
     <main>
+<<<<<<< HEAD
     <?php require('navbar.html'); ?>
         
+=======
+        <?php foreach($posts as $post) : ?>
+>>>>>>> 3c34d2ea00b7e482039db08134fb83a63a92cc91
         <div class="cardtitle">
             <div class="square1">
-                <h1><b>“Alpine ou Honda”, FIA confia que Andretti terá acordo de motor na Fórmula 1</b></h1>
+                <h1><b><?= $post->titulo ?></b></h1>
             </div>
         </div>
         
         <div class="postimage">
-            <img src="/public/assets/Imagemteste.jpg">
+            <img src="/<?=$post->imagem?>">
         </div> 
 
         <div class="cardposit">
@@ -33,30 +40,19 @@
                     <div class="linhacinza">
                         <div class="post">
                             <p>
-                                O presidente da FIA, Mohammed Ben Sulayem, expressou sua confiança de que as regulamentações da Fórmula 1 garantirão que a equipe Andretti tenha um acordo de motor em vigor se acabar entrando na categoria.
-
-                                A equipe Andretti, liderada por Michael Andretti, filho do Campeão de Pilotos de F1 de 1978, Mario Andretti, passou com sucesso pelo primeiro obstáculo para garantir um lugar na F1 quando a FIA aceitou sua proposta. Agora, a entidade americana está sujeita à análise da Formula One Management, enquanto busca aprovação comercial para ingressar na F1 a partir de 2025 ou 2026.
-                                
-                                A Andretti fez parceria com a marca General Motors Cadillac para sua candidatura, uma decisão que dividiu opiniões no paddock da F1. No entanto, a perspectiva de uma unidade de potência Cadillac no futuro é promissora.
-                                
-                                No entanto, uma unidade de potência Cadillac ainda está longe de se tornar realidade, e a Andretti precisa de um acordo de motor com um fornecedor existente para iniciar sua jornada na F1. Inicialmente, havia um acordo com a Alpine, mas Bruno Famin, diretor interino da equipe franco-inglesa, afirmou que tal acordo expirou.
-                                
-                                Ben Sulayem destacou a abordagem que a FIA seguirá: “No início, a Andretti terá que concordar com um dos dois motores. Funciona assim, com as regras, ninguém pode dizer não a eles. Se todas as equipes disserem não, a FIA tem o poder de prosseguir e dizer que pelo menos dois fabricantes de motores estão sendo usados, então os colocamos em um sorteio, e pegamos um. Não é segredo, e tenho certeza de que é ou a Alpine ou a Honda, e uma delas ganharia, porque são as regras.”
-                                
-                                Isso deixa a Alpine e a Honda como potenciais fornecedoras de motores para a Andretti, que precisaria garantir um acordo até 1º de junho um ano antes de sua entrada na F1 (por exemplo, 1º de junho de 2024, se ingressar na temporada de 2025 ou 1º de junho de 2025, se ingressar na temporada de 2026).
-                                
-                                Apesar de a Andretti ter sido vinculada anteriormente à Alpine para um acordo de motor na F1, o retorno da Honda em 2026 oferece uma alternativa intrigante, dada a relação existente entre a equipe e a montadora japonesa, que já fornece motores para a Andretti na IndyCar.
-                                
-                                No entanto, o objetivo de longo prazo da Andretti continua sendo uma unidade de potência da GM/Cadillac, e Ben Sulayem está confiante de que tal conquista pode ser alcançada e que seria benéfica para o esporte.
-                                
-                                “Estou otimista com a GM trazendo a unidade de potência,” disse ele. “Estou muito otimista, não apenas otimista. Nos últimos 20 meses, tivemos duas grandes montadoras, a Audi e a Andretti/GM, e estamos no caminho certo para ter uma unidade de potência da Cadillac, o que é uma conquista.”
+                                <?= $post->conteudo ?>
                             </p>
                             <div class="opinfo">
+                                <?php foreach($users as $user) : ?>
+                                    <?php if($post->autor == $user->id): ?>
                                 <p>
-                                    Gabriel Gavinelli
+    
+                                    <?= $user->nome ?>    
                                 </p>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
                                 <p>
-                                    18 de Janeiro de 2023
+                                    <?= $post->data ?>
                                 </p>
                             </div>
                         </div>
@@ -64,6 +60,7 @@
                 </div>
             </div>
         </div>
+        <?php endforeach; ?>
     </main>
     <?php require('footer.html'); ?>
 </body>
