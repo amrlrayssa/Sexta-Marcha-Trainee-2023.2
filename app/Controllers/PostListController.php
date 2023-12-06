6ea1e3db09d25caa9a911d31c98bfa2f23241e3b
LoweComp <terci_300@outlook.com>
2023-12-06 16:19:58 -0300

    Paging Postlist e ajustes

--- app/Controllers/PostListController.php
+++ app/Controllers/PostListController.php
@@ -46,10 +46,39 @@ class PostListController
     }
 
     public function search(){
+
+        $page = 1;
+        
+        if(isset($_GET['pagina']) && !empty($_GET['pagina']))
+        {
+            $page = intval($_GET['pagina']);
+
+            if($page < 1)
+            {
+                return redirect('admin/lista_posts');
+            }
+        }
+
+        $items_per_page = 5;
+        $start_limit = ($items_per_page * $page) - $items_per_page;
+        $rows_count = App::get('database')->countAll('posts');
+
+        if($start_limit > $rows_count)
+        {
+            return redirect('admin/lista_posts');
+        }
+
+        $total_pages = ceil($rows_count / $items_per_page);
+
         $titulo_buscado = $_GET['search'];
         $posts = App::get('database')->searchPost($titulo_buscado);
+        $tables=[
+            'posts' => $posts,
+            'page' => $page,
+            'total_pages' => $total_pages,
+        ];
 
-        return view('site/postlist', compact('posts'));
+        return view('site/postlist', $tables);
     }
 }
 
--- app/views/site/PostList.view.php
+++ app/views/site/PostList.view.php
@@ -11,25 +11,25 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel = "stylesheet" href = "/public/css/PostList.css">
-    <title> Sexta Marcha - Lista de posts </title>
+    <title> Sexta Marcha - Lista de Posts </title>
 </head>
 <body>
     <?php require('navbar.html'); ?>
     <div class = "left-side">
 
         <div class = "header">
-        <div class = "header_plus">
-            <h1>POSTS MAIS RELEVANTES</h1>
-        </div>    
-        <div class="Searchbar">
-            <div class="icons">
-                <i class = "fa-solid fa-magnifying-glass" id = "search-icon"></i>
-                <form id = "search-form" action = "buscapost" method = "GET">
-                    <input type = "text" name = "search" id = "search-input" placeholder = "Search...">
-                    <button type = "submit" style = "display: none;" ></button>
-                </form>
+            <div class = "header_plus">
+                <h1>POSTS MAIS RELEVANTES</h1>
+            </div>    
+            <div class="Searchbar">
+                <div class="icons">
+                    <i class = "fa-solid fa-magnifying-glass" id = "search-icon"></i>
+                    <form id = "search-form" action = "buscapost" method = "GET">
+                        <input type = "text" name = "search" id = "search-input" placeholder = "Search...">
+                        <button type = "submit" style = "display: none;" ></button>
+                    </form>
+                </div>
             </div>
-        </div>
         
         <?php foreach($posts as $post): ?>
             <div class = "conteinerf">
@@ -46,39 +46,12 @@
                             <input type="hidden" name="id" value="<?= $post->id ?>">
                             <button type="submit" class = "LeiaMais"> LER MAIS ></button>
                         </div>
-                        
                     </div>
-                    
                     </form>
                 </div>
-          
-        </div>
+            </div>
         <?php endforeach; ?>  
     </div>
-
-    <div class="pagi">
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                        <li class="page-item">
-                        <a class="page-link" href="?pagina=<?= $page > 1 ? $page - 1 : 1 ?>" aria-label="<<">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                        </li>
-                        <?php for ($page_number = 1; $page_number <= $total_pages; $page_number++) : ?>
-                        <li class="page-item">
-                            <a class="page-link text-dark" href="?pagina=<?= $page_number ?>">
-                                <?= $page_number ?>
-                            </a>
-                        </li>
-                        <?php endfor; ?>
-                        <li class="page-item">
-                        <a class="page-link" href="?pagina=<?= $page < $total_pages ? $page + 1 : $total_pages ?>" aria-label=">>">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
     
     <div class = "right-side" >
         
@@ -100,6 +73,30 @@
     
 </div> 
 
+    <div class="pagi">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="?pagina=<?= $page > 1 ? $page - 1 : 1 ?>" aria-label="<<">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                    </li>
+                    <?php for ($page_number = 1; $page_number <= $total_pages; $page_number++) : ?>
+                    <li class="page-item">
+                        <a class="page-link text-dark" href="?pagina=<?= $page_number ?>">
+                            <?= $page_number ?>
+                        </a>
+                    </li>
+                    <?php endfor; ?>
+                    <li class="page-item">
+                        <a class="page-link" href="?pagina=<?= $page < $total_pages ? $page + 1 : $total_pages ?>" aria-label=">>">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+    </div>
+        <?php require('footer.html'); ?>  
 <!-- Fazendo o Icone de Botão de Pesquisa -->
 <script>
     document.getElementById('search-icon').addEventListener('click', function() {
@@ -109,8 +106,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 <script src = "https://kit.fontawesome.com/46c6a48072.js" crossorigin = "anonymous"></script>
-<?php require('footer.html'); ?>    
+  
 </body>
 
 </html>
-
