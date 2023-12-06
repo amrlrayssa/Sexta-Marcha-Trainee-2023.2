1d313f4f590a5a8387439d2164b45cf9f84f133e
DaviPacheco <davipacheco10@gmail.com>
2023-12-05 23:37:56 -0300

    Landing Page Overview and Responsivity update

--- app/routes.php
+++ app/routes.php
@@ -1,37 +1,38 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Core\Router;
 
 
-    //Lista de posts
-    $router->get('admin/lista_posts', 'AdminController@view');
-    $router->post('admin/lista_posts/create','AdminController@create');
-    $router->post('admin/lista_posts/delete','AdminController@delete');
-    $router->post('admin/lista_posts/update','AdminController@edit');
+//Lista de posts
+$router->get('admin/lista_posts', 'AdminController@view');
+$router->post('admin/lista_posts/create', 'AdminController@create');
+$router->post('admin/lista_posts/delete', 'AdminController@delete');
+$router->post('admin/lista_posts/update', 'AdminController@edit');
 
-    //Lista de usuarios
-    $router->get('admin/lista_usuarios', 'UserController@view');
-    $router->post('admin/lista_usuarios/create', 'UserController@create');
-    $router->post('admin/lista_usuarios/delete', 'UserController@delete');
-    $router->post('admin/lista_usuarios/update', 'UserController@edit');
+//Lista de usuarios
+$router->get('admin/lista_usuarios', 'UserController@view');
+$router->post('admin/lista_usuarios/create', 'UserController@create');
+$router->post('admin/lista_usuarios/delete', 'UserController@delete');
+$router->post('admin/lista_usuarios/update', 'UserController@edit');
 
-    //post individual
-    $router->get('pvu' , 'pvuController@view');
+//post individual
+$router->get('pvu', 'pvuController@view');
 
-    //landing page
-    $router->get('' , 'landing_pageController@view');
+//landing page
+$router->get('', 'landing_pageController@view');
 
-    //login
-    $router->get('login', 'LoginController@view');
-    $router->post('logon', 'LoginController@login');
-    $router->get('logout', 'LoginController@logout');
+//login
+$router->get('login', 'LoginController@view');
+$router->post('logon', 'LoginController@login');
+$router->get('logout', 'LoginController@logout');
 
-    //dashboard
-    $router -> get('admin_dashboard', 'dashboardController@view');
+//dashboard
+$router->get('admin_dashboard', 'dashboardController@view');
 
-    //post list
-    $router->get('postlist', 'PostListController@view');
-    $router->get('buscapost', 'PostListController@search');
+//post list
+$router->get('postlist', 'PostListController@view');
+$router->get('buscapost', 'PostListController@search');
 
 ?>
