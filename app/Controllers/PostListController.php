7451eba01e9884306aae27f8789e5c4e0d956000
Felipe Lima Nogueira <felipe.nogueira@estudante.ufjf.br>
2023-12-06 02:45:22 -0300

    mudanças

--- app/Controllers/PostListController.php
+++ app/Controllers/PostListController.php
@@ -10,11 +10,36 @@ class PostListController
 
     public function view()
     {
-        $posts = App::get('database')->selectAll('posts');
+        $page = 1;
+        
+        if(isset($_GET['pagina']) && !empty($_GET['pagina']))
+        {
+            $page = intval($_GET['pagina']);
+
+            if($page < 1)
+            {
+                return redirect('admin/lista_posts');
+            }
+        }
+
+        $items_per_page = 5;
+        $start_limit = ($items_per_page * $page) - $items_per_page;
+        $rows_count = App::get('database')->countAll('posts');
+
+        if($start_limit > $rows_count)
+        {
+            return redirect('admin/lista_posts');
+        }
+
+        $total_pages = ceil($rows_count / $items_per_page);
+
+        $posts = App::get('database')->selectAll('posts', $start_limit, $items_per_page);
         $users = App::get('database')->selectAll('users');
         $tables = [
             'posts' => $posts,
             'users' => $users,
+            'page' => $page,
+            'total_pages' => $total_pages,
         ];
 
         return view('/site/postlist', $tables);
